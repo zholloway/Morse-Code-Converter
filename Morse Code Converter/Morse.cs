62bcfc97c184474ba5250bd7f20f408aab869428
zholloway <zholloway92@gmail.com>
2017-03-29 16:59:57 -0400

    dictionary created

--- Morse Code Converter/Morse.cs
+++ Morse Code Converter/Morse.cs
@@ -3,12 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Morse_Code_Converter
 {
     class Morse
     {
-       public const string path = "morse.csv";
+        public const string path = "morse.csv";
 
-    }
+        public string Letter { get; set; }
+
+        public string Code { get; set; }
+
+        public static Dictionary<string, string> MorseDecoderDictionary()
+        {
+            var morseDecoderDictionary = new Dictionary<string, string>();
+
+            using (var reader = new StreamReader(Morse.path))
+            {
+                while (reader.Peek() > -1)
+                {
+                    var letterAndCode = reader.ReadLine();
+                    Console.WriteLine(letterAndCode);
+                    var split = letterAndCode.Split(',');
+                    var letter = split[0];
+                    var code = split[1];
+                    morseDecoderDictionary.Add(letter, code);
+                }
+            }
+
+            return morseDecoderDictionary;
+        }
+    } 
 }
--- Morse Code Converter/Program.cs
+++ Morse Code Converter/Program.cs
@@ -11,18 +11,7 @@ namespace Morse_Code_Converter
     {
         static void Main(string[] args)
         {
-            var morseDecoderDictionary = new Dictionary<string, string>();
-
-            using(var reader = new StreamReader(Morse.path))
-            {
-                while (reader.Peek() > -1)
-                {
-                    var letterAndCode = reader.ReadLine();
-                    Console.WriteLine(letterAndCode);
-                    //var split = letterAndCode.Split(",");
-
-                }
-            }
+            Dictionary<string, string> morseDecoderDictionary = Morse.MorseDecoderDictionary();
 
             Console.WriteLine("Please write a sentence. I will translate it to Morse code!");
             var userString = Console.ReadLine();
