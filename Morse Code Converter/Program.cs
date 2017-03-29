287fb984ea247275355a62e2ac7093c2559c58ae
zholloway <zholloway92@gmail.com>
2017-03-29 16:42:01 -0400

    path established.

--- Morse Code Converter/Program.cs
+++ Morse Code Converter/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Morse_Code_Converter
 {
@@ -10,6 +11,21 @@ namespace Morse_Code_Converter
     {
         static void Main(string[] args)
         {
+            var morseDecoderDictionary = new Dictionary<string, string>();
+
+            using(var reader = new StreamReader(Morse.path))
+            {
+                while (reader.Peek() > -1)
+                {
+                    var letterAndCode = reader.ReadLine();
+                    Console.WriteLine(letterAndCode);
+                    //var split = letterAndCode.Split(",");
+
+                }
+            }
+
+            Console.WriteLine("Please write a sentence. I will translate it to Morse code!");
+            var userString = Console.ReadLine();
         }
     }
 }
